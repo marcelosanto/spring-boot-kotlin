e6bef6b4ad05683e91ef13436bf06c2af373cfbf
Marcelo Santos <rocha.mer21@gmail.com>
2021-08-14 22:54:12 -0300

    criado o PostCustomerRequest e alterado o status do metodo Post

--- src/main/kotlin/com/mercadolivro/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/mercadolivro/controller/CustomerController.kt
@@ -1,9 +1,9 @@
 package com.mercadolivro.mercadolivro.controller
 
+import com.mercadolivro.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.mercadolivro.model.CustomerModel
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customer")
@@ -12,5 +12,11 @@ class CustomerController {
     @GetMapping
     fun getCustomer(): CustomerModel {
         return CustomerModel("1", "Marcelo", "rocha.mer21@gmail.com")
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+         println(customer)
     }
 }
---
+++ src/main/kotlin/com/mercadolivro/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -0,0 +1,6 @@
+package com.mercadolivro.mercadolivro.controller.request
+
+data class PostCustomerRequest (
+    var name: String,
+    var email: String
+        )
