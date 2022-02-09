49f250ef9a02de1c8162cac37240011291b238cf
Marcelo Santos <rocha.mer21@gmail.com>
2022-02-09 11:18:58 -0300

    add flyway

--- src/main/kotlin/com/mercadolivro/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/mercadolivro/model/CustomerModel.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.mercadolivro.model
 
+import org.hibernate.Hibernate
 import javax.persistence.*
 
 @Entity(name = "customer")
@@ -16,4 +17,19 @@ data class CustomerModel (
     var email: String
 
 
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as CustomerModel
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = 0
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id )"
+    }
+}
